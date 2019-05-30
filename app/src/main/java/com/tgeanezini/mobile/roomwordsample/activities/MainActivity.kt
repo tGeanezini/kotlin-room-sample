3b68d2f2e6ac358a94f7fcf90630e4d2d4540fe3
Tiago Geanezini <tiago.geanezini@inlog.com.br>
2019-05-30 17:20:25 -0300

    feat(room): added Entity, Dao and Database

--- app/src/main/java/com/tgeanezini/mobile/roomwordsample/activities/MainActivity.kt
+++ app/src/main/java/com/tgeanezini/mobile/roomwordsample/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.tgeanezini.mobile.roomwordsample
+package com.tgeanezini.mobile.roomwordsample.activities
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import com.tgeanezini.mobile.roomwordsample.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
---
+++ app/src/main/java/com/tgeanezini/mobile/roomwordsample/database/WordRoomDatabase.kt
@@ -0,0 +1,33 @@
+package com.tgeanezini.mobile.roomwordsample.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.tgeanezini.mobile.roomwordsample.models.Word
+
+@Database(entities = [Word::class], version = 1)
+abstract class WordRoomDatabase : RoomDatabase() {
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null)
+                return tempInstance
+
+            synchronized(this) {
+                //Cria a database
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "Word_database").build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
